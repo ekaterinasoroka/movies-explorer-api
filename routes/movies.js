1d131a4e69416288dc9cd562a1df2e4e029f56f5
Ekaterina Soroka <marusyakate@mail.ru>
2022-10-06 21:18:28 +0300

    add joi validation-2

--- routes/movies.js
+++ routes/movies.js
@@ -5,19 +5,10 @@ const {
   validateCreateMovie,
   validateDeleteMovie,
 } = require('../middlewares/validationJoi');
-const {
-  validateCreateMovie,
-  validateDeleteMovie,
-} = require('../middlewares/validationJoi');
 
 moviesRoutes.use(express.json());
-<<<<<<< HEAD
 moviesRoutes.get('/movies', getMovies);
 moviesRoutes.post('/movies', validateCreateMovie, createMovie);
-=======
-moviesRoutes.get('/movies', validateCreateMovie, getMovies);
-moviesRoutes.post('/movies', createMovie);
->>>>>>> 6e7d0e030cf482bb468cc4b63790f91519f15668
 moviesRoutes.delete('/movies/:movieId', validateDeleteMovie, deleteMovieById);
 
 module.exports = moviesRoutes;
