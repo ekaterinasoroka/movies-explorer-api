ab16c8d918419741904ceb9f90fb04fe4ad49f95
Ekaterina Soroka <marusyakate@mail.ru>
2022-11-06 11:57:16 +0300

    level-2 add url

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://evsoroka.nomoredomains.icu', 'https://evsoroka.nomoredomains.icu'],
+    origin: ['http://evsoroka.nomoredomains.icu', 'https://evsoroka.nomoredomains.icu', 'https://api.nomoreparties.co/beatfilm-movies'],
     credentials: true,
   }),
 );
